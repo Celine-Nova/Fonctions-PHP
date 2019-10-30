6e3827245683b0364260b81a60f727ad29fe346c
unknown <cocotte243@gmail.com>
2019-10-30 19:57:57 +0100

    ajout de fonctions

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Hello-world Les functions PHP 
-Ceci est un entraînement aux fontions
+Ceci est un entraînement aux fonctions
 1. Hello World
 2. Mutliplication avec ou sans constante
 3. Concatenation avec $variable ou en dur
 4. Condition paremetre  "string" ou  "int"
 5. Parametre retourne son argument
+6. le nombre le plus grand
+7. Remplacer les chiffre par des lettres
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "hello";
+echo "Hello tout le monde nous sommes le :  " . date('d F Y');
 
 echo PHP_EOL;
 //  1. function Hello World!
@@ -32,7 +32,7 @@ echo "3. methode 2 : Elle s'appelle ". concatenation($prenom, $nom);
 
 echo PHP_EOL;
 // 4. Condition parametre abc ou 123
-function conditionParametre($param){
+function conditionParameter($param){
 
     if (is_string($param)){
         return "abc";
@@ -41,11 +41,32 @@ function conditionParametre($param){
         return "123";
     }
 }
-echo "4. je suis une chaine de caractère ". conditionParametre('string') . " mais je peux aussi être un nombre " . conditionParametre(5);
+echo "4. je suis une chaine de caractère ". conditionParameter('string') . " mais je peux aussi être un nombre " . conditionParameter(5);
 echo PHP_EOL;
 
 // 5. function qui retourne son argument
-function jeRetourneMonArgument($arg){
+function returnMyArgument($arg){
     return $arg;
 }
-echo jeRetourneMonArgument("5. Je peux ecire ce que je veux dans mon parametre");
+echo returnMyArgument("5. Je peux ecire ce que je veux dans mon parametre");
+
+// 6. le nombre le plus grand
+echo PHP_EOL;
+$number1 = 6984;
+$number2 = 454;
+echo "Qui est plus grand" . $number1 . " ou " . $number2 ." ? ";
+function bigger($number1, $number2){
+    return (max($number1, $number2));
+}
+echo PHP_EOL;
+echo "6. " . bigger(6984, 454) ." est plus grand que " . $number2;
+
+echo PHP_EOL;
+// 7. Remplacer les chiffres par des lettres
+function replaceNumber($chaine){
+
+    return strtr($chaine, "013", "oie");
+
+}
+
+echo "7. " . replaceNumber("L3s c0urs d3 pr0grammat10n W3b s0nt tr0p c00l, j3 r3mplace l3s ch1ffr3s par d3s l3ttres");
